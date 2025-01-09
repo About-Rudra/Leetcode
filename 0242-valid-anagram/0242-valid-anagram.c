1ea5184a1fabe3da8dd2eb5b1faa294ad78bc65e
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2025-01-09 07:09:14 +0530

    Time: 3 ms (30.33%), Space: 8.2 MB (50.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -1,22 +1,18 @@
 bool isAnagram(char* s, char* t) {
-    int map[128] = {0};
-    int i;
-
-    if (strlen(s) != strlen(t)) {
+    int m=strlen(t);
+    int n=strlen(s);
+    if(m!=n){
         return false;
     }
-
-    for(i=0;s[i]!='\0';i++){
-        map[s[i]-97]+=1;
+    int count1[26]={0}, count2[26] ={0};
+    for(int i=0; s[i]!='\0';i++){
+       count1[s[i]-'a']++;
+       count2[t[i]-'a']++;
     }
-
-    for(i=0;t[i]!='\0';i++){
-        map[t[i]-97]-=1;
-
-        if( map[t[i]-97]<0){
+    for(int i=0;i<26;i++){
+        if(count1[i]!=count2[i]){
             return false;
         }
-       
     }
     return true;
 }
