814a3046325bdc8cbf9c6ab677e2424ce2661a8d
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2025-01-11 09:23:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (22.16%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+        if(s.length()!=goal.length())return false;
+
+        string concat = s + s;
+
+        if(concat.find(goal) != string::npos){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
