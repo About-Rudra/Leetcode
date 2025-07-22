7a443e00f44ff8bb68073fd50967590a67e0ad7e
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2025-07-22 06:19:57 +0530

    Time: 3 ms (19.91%), Space: 8.4 MB (40.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,22 +1,20 @@
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int> seen;
-
-        while (n != 1 && !seen.count(n)) {
-            seen.insert(n);
-            int sum = 0;
-            while (n > 0) {
-                int digit = n % 10;
-                sum += digit * digit;
-                n /= 10;
-            }
-            n = sum;
+    int SquareSum(int n){
+        int sum = 0;
+        while( n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
-
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set<int>seen;
+        while(n != 1 && ! seen.count(n)){
+            seen.insert(n);
+            n = SquareSum(n);
+        }
         return n == 1;
     }
 };
