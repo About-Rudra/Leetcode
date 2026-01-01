34c2a9cf891a0cc0fd286c39820700a5861b7608
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-01-01 18:13:46 +0530

    Time: 4 ms (72.17%), Space: 14.9 MB (36.54%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,18 +1,39 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         unordered_map<char, int> freq;
+
+//         for (char c : s) {
+//             freq[c]++;
+//         }
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (freq[s[i]] == 1) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> freq;
-
-        for (char c : s) {
-            freq[c]++;
+        unordered_map<char, int> mp;
+        int count = 0;
+        for(char ch : s){
+            mp[ch]++;
         }
 
-        for (int i = 0; i < s.size(); i++) {
-            if (freq[s[i]] == 1) {
-                return i;
-            }
-        }
+       for(int i = 0; i<s.size(); i++){
+        if(mp[s[i]] == 1) return i;
+       }
 
         return -1;
+
     }
+
+       
 };
