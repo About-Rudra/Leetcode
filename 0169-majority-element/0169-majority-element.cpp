266ce6312e80bbb0791a62d1eb9ef36e7e3ab7f0
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-01-01 18:45:36 +0530

    Time: 4 ms (19.04%), Space: 28.4 MB (7.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> count;
+//        for(int i = 0; i<nums.size(); i++){
+//         count[nums[i]] ++;
+
+//         if(count[nums[i]] > nums.size()/2){
+//             return nums[i];
+//         }
+//        }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> count;
-       for(int i = 0; i<nums.size(); i++){
-        count[nums[i]] ++;
+        unordered_map<int, int> mp;
 
-        if(count[nums[i]] > nums.size()/2){
-            return nums[i];
+        for(auto it : nums){
+            mp[it]++;
         }
-       }
+
+        for(auto i : mp){
+            if(i.second > nums.size()/2) return i.first;
+        }
         return -1;
     }
 };
