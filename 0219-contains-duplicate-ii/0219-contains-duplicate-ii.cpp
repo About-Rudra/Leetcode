8867b9a9b1da9dc5b3cdddea1ea0f4851100d778
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-01-02 17:07:32 +0530

    Time: 67 ms (90.24%), Space: 98.8 MB (15.09%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i< nums.size(); i++){
+            int num = nums[i];
+
+            if(mp.count(num) && i - mp[num] <= k){
+                return true;
+            }
+
+            mp[num] = i; // we are storing last found index in the value of hashmap
+        }
+
+        return false;
+        
+    }
+};
