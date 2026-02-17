9dc9cbf5391ec82c96719981dd355eef9f7d68ff
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-02-17 11:56:06 +0530

    Time: 0 ms (100%), Space: 13.2 MB (59.77%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int n = s.length();
-        int vowelCount = 0;
-        int maxVowels = 0;
-        
-       
-        auto isVowel = [](char c) {
-            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-        };
-        
-        
-        for(int i = 0; i < k; i++) {
-            if(isVowel(s[i])) {
-                vowelCount++;
+        bool isVowel[26] = {0};
+        isVowel['a' - 'a'] = true;
+        isVowel['e' - 'a'] = true;
+        isVowel['i' - 'a'] = true;
+        isVowel['o' - 'a'] = true;
+        isVowel['u' - 'a'] = true;
+
+        int l = 0, curr = 0, res = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            if (isVowel[s[r] - 'a']) curr++;
+
+            if (r - l + 1 > k) {
+                if (isVowel[s[l] - 'a']) curr--;
+                l++;
             }
+
+            res = max(res, curr);
         }
-        
-        
-        maxVowels = vowelCount;
-        
-        // Step 3: Slide the window
-        for(int i = k; i < n; i++) {
-            // Remove leftmost character from window
-            if(isVowel(s[i - k])) {
-                vowelCount--;
-            }
-            
-            // Add new character to window
-            if(isVowel(s[i])) {
-                vowelCount++;
-            }
-            
-            // Update maximum
-            maxVowels = max(maxVowels, vowelCount);
-            
-            
-        }
-        
-        return maxVowels;
+
+        return res;
     }
 };
