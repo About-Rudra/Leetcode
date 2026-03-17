9442f44133b15b078a886a11895f4fa7fc9488af
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-03-17 20:43:37 +0530

    Time: 0 ms (100%), Space: 113.8 MB (60.62%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,26 +1,48 @@
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) {
+//         double maxavg = INT_MIN;
+//         double avg = 0;
+//         double sum = 0;
+
+//         for(int i = 0; i<k; i++){
+//             sum += nums[i];
+//         }
+//         avg = sum / k;
+
+//         maxavg = max(avg, maxavg);
+
+//         for(int i = k; i< nums.size(); i++){
+//             sum += nums[i];
+//             sum -= nums[i-k];
+//             avg = sum / k;
+
+//             maxavg = max(avg, maxavg);
+//         }
+
+//         return maxavg;
+        
+//     }
+// };
+
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double maxavg = INT_MIN;
-        double avg = 0;
         double sum = 0;
-
-        for(int i = 0; i<k; i++){
-            sum += nums[i];
-        }
-        avg = sum / k;
-
-        maxavg = max(avg, maxavg);
-
-        for(int i = k; i< nums.size(); i++){
-            sum += nums[i];
-            sum -= nums[i-k];
-            avg = sum / k;
-
-            maxavg = max(avg, maxavg);
-        }
-
-        return maxavg;
         
+        
+        for(int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        
+        double maxSum = sum;
+        
+        
+        for(int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = max(maxSum, sum);
+        }
+        
+        return maxSum / k;  // Divide only at the end
     }
 };
