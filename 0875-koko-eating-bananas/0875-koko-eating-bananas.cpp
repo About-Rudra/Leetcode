00903df7953efaa3c9914bc71ae2c96733efd29c
About-Rudra <116325707+About-Rudra@users.noreply.github.com>
2026-04-01 09:52:13 +0530

    Time: 20 ms (6.72%), Space: 23 MB (12.32%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+    bool caneatintime(vector<int>& piles, int h, int speed){
+        int hours = 0;
+
+        for(int pile : piles){
+            hours += ceil((double)pile / speed);
+        }
+
+        return hours <= h;
+    }
+
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int minsp = 1, maxsp = 0;
+
+        for(int pile : piles){
+            maxsp = max(maxsp, pile);
+        }
+
+        while(minsp < maxsp){
+            int mid = minsp + (maxsp - minsp) /2;
+
+            if(caneatintime(piles, h, mid)){
+                maxsp = mid;
+            }else{
+                minsp = mid+1;
+            }
+        }
+
+        return minsp;
+        
+    }
+};
